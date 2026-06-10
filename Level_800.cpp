8e46f87dd1ecae0c4e2fedb08ebaaa2d630073f0
doshanlal-coder <doshanlals@gmail.com>
2026-06-10 19:21:41 +0530

    Initial commit

---
+++ Level_800.cpp
@@ -0,0 +1,16 @@
+//1//Watermelon
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     int w;
+//     cin>>w;
+//     cout<<((w>2 and w<=100 and w%2==0)? "YES" : "NO");
+//     return 0;
+// }
+
+
+
+
+
+//2//
