ea3d0b777854d7366a82bcc2fe9c98bbb40e8c90
doshanlal-coder <doshanlals@gmail.com>
2026-06-11 16:08:02 +0530

    CP

---
+++ Level_1000.cpp
@@ -0,0 +1,12 @@
+//1//Theatre square
+
+// #include <iostream>
+// using namespace std;
+// int main (){
+//     long long n,m,a;
+//     cin>>n>>m>>a;
+//     cout<<(((n%a==0)? n/a: n/a+1) * ((m%a==0)? m/a: m/a+1));
+//     return 0;
+// }
+
+//2//
--- Level_800.cpp
+++ Level_800.cpp
@@ -13,4 +13,62 @@
 
 
 
-//2//
+//2//Way Too Long Words
+
+// #include <iostream>
+// #include <string>
+// using namespace std;
+// int main (){
+//     int n;
+//     cin>>n;
+//     while(n--){
+//         string word;
+//         cin>>word;
+//         cout<<((word.length()>10)? word[0]+to_string(word.length()-2) +word[word.length()-1]:word)<<endl;
+//     }
+// }
+
+
+
+//3//Elephants
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     cout<<((x/5) + (x%5)/4 + ((x%5)%4)/3 + (((x%5)%4)%3)/2 + ((((x%5)%4)%3)%2)/1);
+//     return 0;
+// }
+
+// //OR
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int x;
+//     cin >> x;
+//     cout << ((x % 5 == 0) ? x / 5 : x / 5 + 1) << endl;
+    
+//     return 0;
+// }
+
+//4//Team
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n,Petya, Vasya, Tonya;
+//     cin>>n;
+//     int p=0;
+//     for(int i=1; i<=n; i++){
+//         cin>>Petya>>Vasya>>Tonya;
+//         if(Petya+Vasya+Tonya >=2){
+//             p++;
+//         }
+//     }
+//     cout<<p;
+//     return 0;
+// }
+
+//5//
