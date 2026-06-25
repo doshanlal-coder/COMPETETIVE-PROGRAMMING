b3efa55d2bf2e9a3da3da1995b2cef607cf29a8b
doshanlal-coder <doshanlals@gmail.com>
2026-06-25 20:57:06 +0530

    CP

--- Level_900.cpp
+++ Level_900.cpp
@@ -93,21 +93,21 @@
 
 //Problem//Gravity flip
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int main (){
-    int n;
-    cin>>n;
-    vector <int> vec(n);
-    for(int i=0; i<n; i++){
-        cin>>vec[i];
-    }
-    sort(vec.begin(),vec.end());
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// int main (){
+//     int n;
+//     cin>>n;
+//     vector <int> vec(n);
+//     for(int i=0; i<n; i++){
+//         cin>>vec[i];
+//     }
+//     sort(vec.begin(),vec.end());
 
-    for(int i=0; i<n; i++){
-        cout<<vec[i]<<" ";
-    }
-    return 0;
-}
+//     for(int i=0; i<n; i++){
+//         cout<<vec[i]<<" ";
+//     }
+//     return 0;
+// }
