07b3f1eb7185b200daa55b7ebca3471aaa42ad1c
doshanlal-coder <doshanlals@gmail.com>
2026-06-26 21:46:00 +0530

    CP

--- Level_900.cpp
+++ Level_900.cpp
@@ -130,3 +130,20 @@
 //     }
 //     return 0;
 // }
+
+//Problem?? HQ9+
+
+#include <iostream>
+using namespace std;
+int main (){
+    string code;
+    cin>>code;
+    for(int i=0; i<code.length();i++){
+        if(code[i]=='H' || code[i]=='Q' || code[i]=='9'){
+            cout<<"YES";
+            return 0;
+        }
+    }
+    cout<<"NO";
+    return 0;
+}
