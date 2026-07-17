bce78dfacf61c67ac53f3f368d954c981b98d2c4
doshanlal-coder <doshanlals@gmail.com>
2026-07-18 00:29:39 +0530

    cp

--- Level_1100.cpp
+++ Level_1100.cpp
@@ -1,22 +1,61 @@
-//Problemm ? TAXI
+// Problemm ? TAXI
 
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main (){
+int main()
+{
     int n;
-    int sum = 0;
-    cin>>n;
-    for(int i = 0 ; i<n ; i++){
+    int one = 0, two = 0, three = 0, four = 0;
+    cin >> n;
+    int taxi = 0;
+    for (int i = 0; i < n; i++)
+    {
         int num;
-        cin>>num;
-        sum += num;
+        cin >> num;
+        if (num == 1)
+        {
+            one++;
+        }
+        if (num == 2)
+        {
+            two++;
+        }
+        if (num == 3)
+        {
+            three++;
+        }
+        if (num == 4)
+        {
+            four++;
+        }
     }
-    if(sum % 4 == 0){
-        
-    cout<<sum/4;
+    taxi+=four;
+    taxi+= two/2;
+    if(three<one){
+        taxi+=three;
+        one -= three;
+        if(two%2 != 0){
+            if(one<=2){
+                taxi++;
+            }
+            else{
+                taxi++;
+                if((one-2)%4 == 0){
+                    taxi = taxi+ (one-2)/4;
+                } else{
+                    taxi = taxi+ ((one-2)/4) + 1;
+                }
+            }
+        }
     }else{
-        
-    cout<<(sum/4) + 1;
+        taxi+=one;
+        three-=one;
+        taxi+=three;
+        if(two%2 != 0){
+            taxi++;
+        }
+
     }
+    cout<<taxi;
     return 0;
 }
